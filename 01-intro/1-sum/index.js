ef070c994d6412fe4fa38a5ab8f408686b2af8e7
Anna Kuzina <anna.derey@mail.ru>
2023-01-20 21:08:43 +0700

    Update index.js
    Вернула комментарии

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -1,3 +1,10 @@
+/**
+ * Sum of two numbers
+ *
+ * @param {number} m first number
+ * @param {number} n second number
+ * @returns {number}
+ */
 export const sum = (m, n) => {
   let result = Number(m) + Number(n);
   return result;
