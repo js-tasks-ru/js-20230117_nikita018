e102f103aaa482d18722eae432cbe164fb4148eb
Anna Kuzina <anna.derey@mail.ru>
2023-01-20 20:25:24 +0700

    Реализовала функцию суммирования двух чисел

--- 01-intro/1-sum/index.js
+++ 01-intro/1-sum/index.js
@@ -1,10 +1,4 @@
-/**
- * Sum of two numbers
- *
- * @param {number} m first number
- * @param {number} n second number
- * @returns {number}
- */
 export const sum = (m, n) => {
-
+  let result = Number(m) + Number(n);
+  return result;
 };
